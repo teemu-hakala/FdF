2143195b98699ccf9d810801c813fdf2cdfca17a
Teemu Hakala <thakala@c1r6p9.hive.fi>
2022-03-24 14:04:15 +0200

    .gitignore, fixed get_next_line.h libft.h inclusion

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:23:04 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/19 12:05:55 by thakala          ###   ########.fr       */
+/*   Updated: 2022/03/24 14:03:11 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 2797
 # define FD_MAX 10240
-# include "libft/includes/libft.h"
+# include "libft.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
