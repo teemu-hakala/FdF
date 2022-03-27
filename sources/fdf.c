f6f506df76da15351bb1b389181f0108482223af
Teemu Hakala <thakala@c1r6p14.hive.fi>
2022-03-27 09:07:16 +0300

    bug fixed, missing mlx->

--- sources/fdf.c
+++ sources/fdf.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 14:07:37 by thakala           #+#    #+#             */
-/*   Updated: 2022/03/27 08:57:30 by thakala          ###   ########.fr       */
+/*   Updated: 2022/03/27 09:06:52 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	init_req(t_mlx *mlx, t_fdf *fdf)
 	mlx->mlx = mlx_init();
 	if (mlx->mlx == NULL)
 		exit_msg("Minilibx initialization failure (is NULL);", EXIT_ERROR);
-	mlx->win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, WIN_NAME);
+	mlx->win = mlx_new_window(mlx->mlx, WIN_WIDTH, WIN_HEIGHT, WIN_NAME);
 	if (mlx->win == NULL)
 		exit_msg("Window pointer is NULL!", EXIT_ERROR);
 	fdf->origin = (t_pt) { .row = WIN_HEIGHT / 2, .col = WIN_WIDTH / 2 };
@@ -29,10 +29,10 @@ void	debug_function(t_mlx *mlx, t_fdf *fdf)
 {
 	int		i;
 
-printf("%d %d %d\n", fdf->origin.col, fdf->origin.row, (int)fdf->color);
 	i = 0;
 	while (i < 100)
 	{
+printf("%d %d %d\n", fdf->origin.col + i, fdf->origin.row + i, (int)fdf->color);
 		mlx_pixel_put(mlx->mlx, mlx->win,
 			fdf->origin.col + i, fdf->origin.row + i, (int)fdf->color);
 		i++;
@@ -47,7 +47,7 @@ int	main(int argc, char **argv)
 	(void) argc;
 	(void) argv;
 	init_req(&mlx, &fdf);
-	debug_function(&mlx, &fdf);
 	hook_all(&mlx, &fdf);
+	debug_function(&mlx, &fdf);
 	mlx_loop(mlx.mlx);
 }
