1b18f42286cfe7b49db33bc3d2ad76399f0a3909
Teemu Hakala <thakala@c2r4p6.hive.fi>
2022-03-28 19:57:39 +0300

    prepend absolute script directory to bash call in debug_remaker.sh and debug_tasks.sh

--- eval_tests/scripts/debug_remaker.sh
+++ eval_tests/scripts/debug_remaker.sh
@@ -3,10 +3,12 @@
 # https://stackoverflow.com/a/246128/16586661
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-file_pre_requisites="../../.pre_requisites"
+file_pre_requisites="${SCRIPT_DIR}/../../.pre_requisites"
 file_build_debug_text="${SCRIPT_DIR}/pre_requisite_detectors/build-debug-text.c.test"
 
-pwd
+echo ${SCRIPT_DIR}
+
+#pwd
 
 diff $file_build_debug_text $file_pre_requisites
 if (($? != 0))
--- eval_tests/scripts/debug_tasks.sh
+++ eval_tests/scripts/debug_tasks.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+
+# https://stackoverflow.com/a/246128/16586661
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 if (( $# == 1 ))
 then
-	diff ../../.pre_requisites pre_requisite_detectors/build-debug-text.c.test
+	diff ${SCRIPT_DIR}/../../.pre_requisites ${SCRIPT_DIR}/pre_requisite_detectors/build-debug-text.c.test
 	if (( $? != 0))
 	then
 		touch ../../.pre_requisites
 		make -C ../../ $1
-		cat pre_requisite_detectors/build-debug-text.c.test > ../../.pre_requisites
+		cat ${SCRIPT_DIR}/pre_requisite_detectors/build-debug-text.c.test > ${SCRIPT_DIR}/../../.pre_requisites
 	else
 		echo "Already \"made with debug.\""
 	fi
