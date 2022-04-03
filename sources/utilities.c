7195d00fa683f3d8609bc9b5b5abc835d3944195
Teemu Hakala <thakala@c1r6p16.hive.fi>
2022-04-03 12:30:15 +0300

    utilities::abs_mx() functions (correctly) now

--- sources/utilities.c
+++ sources/utilities.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 10:22:55 by thakala           #+#    #+#             */
-/*   Updated: 2022/04/03 10:40:30 by thakala          ###   ########.fr       */
+/*   Updated: 2022/04/03 12:29:10 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ double	max(double d0, double d1)
 
 int	abs_max(int i0, int i1)
 {
-	i1 = (i1 < 0) * -1;
-	i0 = (i0 < 0) * -1;
+	i1 *= (i1 < 0) * -1 | 0x1;
+	i0 *= (i0 < 0) * -1 | 0x1;
 	if (i1 > i0)
 		return (i1);
 	return (i0);
